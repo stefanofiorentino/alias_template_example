b5f02e988b417f2039160bc0c71363ed46f1dd59
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-04-14 17:47:58 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+template<class T> using vector = std::vector<T>; // introduced in C++11 (modern??)
+
+int main() {
+        vector<int> values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for(auto const &v : values) {
+            std::cout << v;
+        }
+        std::cout << std::endl;
+    return 0;
+}
+
